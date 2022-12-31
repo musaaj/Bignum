576df0fa8aa9ba17aecf17e0f36bfa37010959d8
mikias <mikiasabiy16@gmail.com>
2022-12-31 09:32:07 -0500

    update: main.c: new test added for the new function is_bn_eq function

--- main.c
+++ main.c
@@ -13,6 +13,10 @@ int main(void)
 	bignum_from_string(&n, "7849377843");
 	bignum_from_string(&m, "78493778433");
 
+
+    /* test for bn_eq */
+    is_bn_eq(n, m, 0, "This is a bn_eq's test");
+
 	is_eq(c(&n, "==", &m), 0, 1, "Equal");
 	is_eq(c(&n, "!=", &m), 1, 2, "Not Equal");
 	is_eq(c(&n, ">=", &m), 0, 3, "Greater than or Equal");
