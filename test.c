9e1ee162c75517929e759ba1a3bd666fed559e76
mikias <mikiasabiy16@gmail.com>
2022-12-31 09:26:46 -0500

    update: test.c: tester for bignum_t instances added

--- test.c
+++ test.c
@@ -23,3 +23,33 @@ void is_eq(int f, int s, int no, char *msg)
 	if (msg != NULL)
 		printf("%s\n", msg);
 }
+
+/**
+ * is_bn_eq - compares to bignum objects and if values are not equal
+ * it will print the passed message elese Test passed and the test
+ * number.
+ * @f: first bignum_t instances.
+ * @s: second bignum_t instances.
+ * @no: test number(index).
+ * @msg: the message to print if not equal.
+ *
+ * Return: Void
+ */
+void is_bn_eq(bignum_t f, bignum_t s, int no, char *msg)
+{
+	char *sf = NULL, *ss = NULL;
+
+	if (c(&f, "==", &s))
+	{
+		printf("%d : Test passed\n", no);
+		return;
+	}
+	printf("%d : Test failed\n", no);
+	sf = bignum_to_string(&f);
+	ss = bignum_to_string(&s);
+	printf("%s != %s\n", sf, ss);
+	free(sf);
+	free(ss);
+	if (msg != NULL)
+		printf("%s\n", msg);
+}
