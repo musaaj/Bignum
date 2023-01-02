8af65bfff85148ec26be8750571914d704a9962d
mikias <mikiasabiy16@gmail.com>
2023-01-01 21:02:33 -0500

    update: main.c: tests updated due to the update to function names

--- main.c
+++ main.c
@@ -7,22 +7,21 @@
  */
 int main(void)
 {
-	bignum_t n;
-	bignum_t m;
+	bignum_t *n;
+	bignum_t *m;
 
-	bignum_from_string(&n, "7849377843");
-	bignum_from_string(&m, "78493778433");
-
+	n = string_to_bignum("7849377843");
+	m = string_to_bignum("78493778433");
 
     /* test for bn_eq */
-    is_bn_eq(n, m, 0, "This is a bn_eq's test");
+	is_bn_eq(*n, *m, 0, "This is a bn_eq's test");
 
-	is_eq(c(&n, "==", &m), 0, 1, "Equal");
-	is_eq(c(&n, "!=", &m), 1, 2, "Not Equal");
-	is_eq(c(&n, ">=", &m), 0, 3, "Greater than or Equal");
-	is_eq(c(&n, "<=", &m), 1, 4, "Less than or Equal");
-	is_eq(c(&n, ">", &m), 0, 5, "Greater than");
-	is_eq(c(&n, "<", &m), 1, 6, "Less than");
+	is_eq(c(n, "==", m), 0, 1, "Equal");
+	is_eq(c(n, "!=", m), 1, 2, "Not Equal");
+	is_eq(c(n, ">=", m), 0, 3, "Greater than or Equal");
+	is_eq(c(n, "<=", m), 1, 4, "Less than or Equal");
+	is_eq(c(n, ">", m), 0, 5, "Greater than");
+	is_eq(c(n, "<", m), 1, 6, "Less than");
 
 	/* add tests if you have some */
 	/* i did not have enogh time to test it */
